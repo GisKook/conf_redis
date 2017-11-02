13ef9ad3ac4683bcef5f663888b09c18cf94df84
zhangkai <zhangkai.gis@163.com>
2017-11-02 13:17:57 +0800

    add get version interface

--- http_srv/handler_get_version.go
+++ http_srv/handler_get_version.go
@@ -15,11 +15,11 @@ const (
 type GetVersionResponse struct {
 	Code    int    `json:"code"`
 	Desc    string `json:"desc"`
-	Version string `json:"version"`
-	Count   string `json:"count"`
+	Version string `json:"version_code"`
+	Count   int    `json:"count"`
 }
 
-func EncodeGetVersionResponse(code int, errmsg string, version string, count string) string {
+func EncodeGetVersionResponse(code int, errmsg string, version string, count int) string {
 	response := &GetVersionResponse{
 		Code:    code,
 		Desc:    errmsg,
@@ -41,9 +41,10 @@ func (s *Server) handler_get_version(w http.ResponseWriter, r *http.Request) {
 	}()
 	dump_requst(r)
 
+	r.ParseForm()
 	t := r.Form.Get(TYPE)
 	if t == "" {
-		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), "缺少参数type type可选值为 ci,black_list,local_number,county_number", "", ""))
+		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), "缺少参数type type可选值为 ci,black_list,local_number,county_number", "", 0))
 		return
 	}
 
@@ -51,18 +52,17 @@ func (s *Server) handler_get_version(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 	v, err := conn.Do("GET", t+VERSION)
 	if err != nil {
-		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), err.Error(), "", ""))
+		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), err.Error(), "", 0))
 		return
 	}
 	version, _ := redis.String(v, nil)
 
 	v, err = conn.Do("SCARD", t)
 	if err != nil {
-		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), err.Error(), version, ""))
+		fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_ERR), err.Error(), version, 0))
 		return
 	}
-	count, _ := redis.String(v, nil)
+	count, _ := redis.Int(v, nil)
 
 	fmt.Fprint(w, EncodeGetVersionResponse(int(HTTP_RESP_SUCCESS), HTTP_RESP_SUCCESS.Desc(), version, count))
-
 }
